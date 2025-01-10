a39bec998e976ec4456629b7d756213bbeea31cf
Antti <antti.tuomola@gofore.com>
2025-01-10 23:02:48 +0200

    Refactor astro.config.mjs to include Vercel adapter and maintain existing integrations. Adjusted formatting for consistency.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,23 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  output: 'server',
-  integrations: [
-    tailwind({
-      // Force Tailwind to generate all styles
-      applyBaseStyles: false
-    }),
-    mdx({
-      syntaxHighlight: 'prism',
-      drafts: true,
-    }),
-    react()
-  ],
-  markdown: {
-    shikiConfig: {
-      theme: 'github-light',
-      wrap: true
-    }
-  }
+	output: 'server',
+	adapter: vercel(),
+	integrations: [
+		tailwind({
+			// Force Tailwind to generate all styles
+			applyBaseStyles: false
+		}),
+		mdx({
+			syntaxHighlight: 'prism',
+			drafts: true,
+		}),
+		react()
+	],
+	markdown: {
+		shikiConfig: {
+			theme: 'github-light',
+			wrap: true
+		}
+	}
 });
